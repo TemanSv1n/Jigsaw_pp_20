a139a27efa2d6743fff50d2ecb8cbdd5a43c9952
Pisyapopa777 <hahariashka@yandex.ru>
2024-01-29 22:24:26 +0700

    uaaaaaa

--- src/main/java/net/svisvi/jigsawpp/init/JigsawPpModBlocks.java
+++ src/main/java/net/svisvi/jigsawpp/init/JigsawPpModBlocks.java
@@ -9,8 +9,10 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.DeferredRegister;
 
 import net.minecraft.world.level.block.Block;
+import net.svisvi.jigsawpp.block.RawTeapotBlock;
 
 public class JigsawPpModBlocks {
     public static final DeferredRegister<Block> REGISTRY = DeferredRegister.create(ForgeRegistries.BLOCKS, JigsawPpMod.MODID);
     public static final RegistryObject<Block> COAL_FOSSIL = REGISTRY.register("coal_fossil", () -> new CoalFossilBlock());
+    public static final RegistryObject<Block> RAW_TEAPOT = REGISTRY.register("raw_teapot", () -> new RawTeapotBlock());
 }
