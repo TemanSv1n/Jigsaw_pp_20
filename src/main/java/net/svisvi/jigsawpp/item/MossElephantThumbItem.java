f3c94287b21a2ff518e060d970994654c38a05b1
TemanSv1n <temasyr.o@gmail.com>
2024-02-01 21:49:25 +0300

    Moss Elephant spawning
    Finally finished the elephant!

--- src/main/java/net/svisvi/jigsawpp/item/MossElephantThumbItem.java
+++ src/main/java/net/svisvi/jigsawpp/item/MossElephantThumbItem.java
@@ -19,6 +19,7 @@ public class MossElephantThumbItem extends Item {
     @Override
     public void appendHoverText(ItemStack itemstack, Level world, List<Component> list, TooltipFlag flag) {
         super.appendHoverText(itemstack, world, list, flag);
+        list.add(Component.translatable("item.jigsaw_pp.moss_elephant_thumb.desc"));
     }
 }
 
--- src/main/java/net/svisvi/jigsawpp/procedures/MossElephantFearProcedure.java
+++ src/main/java/net/svisvi/jigsawpp/procedures/MossElephantFearProcedure.java
@@ -15,24 +15,8 @@ public class MossElephantFearProcedure {
     public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
         if (entity == null)
             return;
-        if (entity.getDeltaMovement().x() > 0.0001) {
-            if (Math.random() <= 0.1) {
-                if (world instanceof Level _level && !_level.isClientSide()) {
-                    ItemEntity entityToSpawn = new ItemEntity(_level, x, y, z, new ItemStack(JigsawPpModItems.SWEET_BREAD.get()));
-                    entityToSpawn.setPickUpDelay(10);
-                    _level.addFreshEntity(entityToSpawn);
-                }
-                if (world instanceof Level _level) {
-                    if (!_level.isClientSide()) {
-                        _level.playSound(null, new BlockPos((int)x, (int)y, (int)z), ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("entity.cow.milk")), SoundSource.NEUTRAL, 1, 1);
-                    } else {
-                        _level.playLocalSound(x, y, z, ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("entity.cow.milk")), SoundSource.NEUTRAL, 1, 1, false);
-                    }
-                }
-            }
-        }
-        if (entity.getDeltaMovement().z() > 0.0001) {
-            if (Math.random() <= 0.1) {
+        if (entity.getDeltaMovement().x() > 0.0001 | entity.getDeltaMovement().z() > 0.0001) {
+            if (Math.random() <= 0.05) {
                 if (world instanceof Level _level && !_level.isClientSide()) {
                     ItemEntity entityToSpawn = new ItemEntity(_level, x, y, z, new ItemStack(JigsawPpModItems.SWEET_BREAD.get()));
                     entityToSpawn.setPickUpDelay(10);
