426b93e18784d7fe44fe5ea9c6d2b0909ec3a533
TemanSv1n <temasyr.o@gmail.com>
2024-02-06 21:48:26 +0300

    Finished Poop Effect
    Added poop overlay and LOGIC for effect is now connected

---
+++ src/main/java/net/svisvi/jigsawpp/event/init/ClientEvents.java
@@ -0,0 +1,10 @@
+package net.svisvi.jigsawpp.event.init;
+
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.common.Mod;
+import net.svisvi.jigsawpp.JigsawPpMod;
+
+public class ClientEvents {
+//    @Mod.EventBusSubscriber(modid = JigsawPpMod.MODID, value = Dist.CLIENT)
+//    public static class ClientModBusEvents
+}
