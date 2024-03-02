0de18337240d3bc2b02cfb4facb57d4ef8be1fb0
scodens <143162887+scodens@users.noreply.github.com>
2024-03-02 20:33:10 +0200

    added drist button

--- src/main/java/net/svisvi/jigsawpp/item/dristButtonItem.java
+++ src/main/java/net/svisvi/jigsawpp/item/dristButtonItem.java
@@ -1,4 +1,26 @@
 package net.svisvi.jigsawpp.item;
 
-public class dristButtonItem {
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.context.UseOnContext;
+
+
+public class dristButtonItem extends Item {
+
+    public dristButtonItem() {
+        super(new Properties().stacksTo(1).rarity(Rarity.RARE));
+    }
+
+    @Override
+    public InteractionResult onItemUseFirst(ItemStack stack, UseOnContext context) {
+        Player player = context.getPlayer();
+        player.getCooldowns().addCooldown(this, 150);
+        String itemName = stack.getDisplayName().getString();
+        System.out.println(itemName);
+        return super.onItemUseFirst(stack, context);
+    }
 }
+
