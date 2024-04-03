b7f50a661b76db86f71e36e4c96913ab8cfcb34c
TemanSv1n <temasyr.o@gmail.com>
2024-04-04 00:13:47 +0300

    Purgen++
    Purgen pilules duration buff + additional effects support
    
    yeah, it's a lil bit COSTYL, but it works actually. Even in the CulinaryConstruct is made like this, what do you want more?!?!

--- src/main/java/net/svisvi/jigsawpp/item/pilule/AdvancedPurgenPiluleItem.java
+++ src/main/java/net/svisvi/jigsawpp/item/pilule/AdvancedPurgenPiluleItem.java
@@ -6,6 +6,8 @@ import net.svisvi.jigsawpp.effect.init.ModEffects;
 public class AdvancedPurgenPiluleItem extends AbstractPiluleItem{
     static int duration = 400;
     int duration_buff = 0;
+    static int amplifier = 1;
+    MobEffectInstance secondary_effect;
 
     @Override
     public int duration(){return this.duration;}
@@ -13,11 +15,10 @@ public class AdvancedPurgenPiluleItem extends AbstractPiluleItem{
     public int duration_buff(){
         return this.duration_buff;
     }
+    public int amplifier(){return this.amplifier;}
+    public MobEffectInstance secondary_effect(){return this.secondary_effect;}
     public AdvancedPurgenPiluleItem(){
         super(new MobEffectInstance(ModEffects.PURGATIVE.get(), duration, 1));
     }
-    public AdvancedPurgenPiluleItem(int durationBuff){
-        super(new MobEffectInstance(ModEffects.PURGATIVE.get(), duration, 0));
-        duration_buff = durationBuff;
-    }
+
 }
