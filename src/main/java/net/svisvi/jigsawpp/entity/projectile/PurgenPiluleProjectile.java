0db0c84bda3d6394ce96ac1a2e3d9040aa6e623a
TemanSv1n <temasyr.o@gmail.com>
2024-04-08 00:36:24 +0300

    Extinguisher dispenser
    finally....
    
    and some purgen gun fixes))

--- src/main/java/net/svisvi/jigsawpp/entity/projectile/PurgenPiluleProjectile.java
+++ src/main/java/net/svisvi/jigsawpp/entity/projectile/PurgenPiluleProjectile.java
@@ -2,6 +2,9 @@ package net.svisvi.jigsawpp.entity.projectile;
 
 import net.minecraft.core.particles.ItemParticleOption;
 import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.network.syncher.EntityDataAccessor;
+import net.minecraft.network.syncher.EntityDataSerializers;
+import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.Entity;
@@ -13,13 +16,20 @@ import net.minecraft.world.entity.projectile.ThrowableItemProjectile;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.EntityHitResult;
 import net.minecraft.world.phys.HitResult;
+import net.minecraft.world.phys.Vec3;
 import net.svisvi.jigsawpp.effect.PoopEffect;
+import net.svisvi.jigsawpp.effect.PurgativeEffect;
 import net.svisvi.jigsawpp.effect.init.ModEffects;
 import net.svisvi.jigsawpp.entity.init.ModEntities;
 import net.svisvi.jigsawpp.item.init.ModItems;
 import net.svisvi.jigsawpp.item.pilule.AbstractPiluleItem;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 //Procedures
 public class PurgenPiluleProjectile extends ThrowableItemProjectile implements ItemSupplier {
@@ -60,7 +70,9 @@ public class PurgenPiluleProjectile extends ThrowableItemProjectile implements I
         super.onHitEntity(pResult);
         pResult.getEntity().hurt(this.damageSources().thrown(this, this.getOwner()), 0.5F);
         if (pResult.getEntity() instanceof LivingEntity ent) {
-            this.getItem().getItem().finishUsingItem(this.getItem(), ent.level(), ent);
+            if (PurgativeEffect.poopAdditionConditionLiquidWay(ent, new MobEffectInstance(ModEffects.PURGATIVE.get(), 21, 0))) {
+                this.getItem().getItem().finishUsingItem(this.getItem(), ent.level(), ent);
+            }
         }
 //        PoopEffect.addEffectLiquidWay(pResult.getEntity(),(new MobEffectInstance(ModEffects.POOP.get(), 60, 1, false, false)));
     }
@@ -79,6 +91,45 @@ public class PurgenPiluleProjectile extends ThrowableItemProjectile implements I
         if (this.level() instanceof ServerLevel _level) {
             _level.sendParticles(ParticleTypes.SMALL_FLAME, this.getX(), this.getY(), this.getZ(), 3, 0.2, 0.2, 0.2, 0);
         }
+
+        if (this.getOwner() == null || this == null){
+            return;}
+
+
+
+        double speeed = 0.5;
+        double x = this.getX();
+        double y = this.getY();
+        double z = this.getZ();
+        Level world = this.level();
+        autoAim(speeed, x, y, z, world, this, this.getOwner());
+
+
+
+    }
+
+    public void autoAim(double speed, double x, double y, double z, Level world, Projectile immediatesourceentity, Entity entity){
+        double dis = 0;
+            final Vec3 _center = new Vec3(x, y, z);
+            List<Entity> _entfound = world.getEntitiesOfClass(Entity.class, new AABB(_center, _center).inflate(3 / 2d), e -> true).stream().sorted(Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_center))).collect(Collectors.toList());
+            for (Entity entityiterator : _entfound) {
+                if (!(immediatesourceentity == entityiterator) && !(entity == entityiterator)) {
+                    if (((Entity) world.getEntitiesOfClass(LivingEntity.class, AABB.ofSize(new Vec3(x, y, z), 3, 3, 3), e -> true).stream().sorted(new Object() {
+                        Comparator<Entity> compareDistOf(double _x, double _y, double _z) {
+                            return Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_x, _y, _z));
+                        }
+                    }.compareDistOf(x, y, z)).findFirst().orElse(null)) == entityiterator) {
+                        dis = Math.sqrt(Math.pow(entityiterator.getX() - immediatesourceentity.getX(), 2) + Math.pow(entityiterator.getY() - immediatesourceentity.getY(), 2) + Math.pow(entityiterator.getZ() - immediatesourceentity.getZ(), 2));
+                        immediatesourceentity.setDeltaMovement(new Vec3((((entityiterator.getX() - immediatesourceentity.getX()) / dis) * speed), (((entityiterator.getY() - immediatesourceentity.getY()) / dis) * speed),
+                                (((entityiterator.getZ() - immediatesourceentity.getZ()) / dis) * speed)));
+                        if (world instanceof ServerLevel _level)
+                            _level.sendParticles(ParticleTypes.SMOKE, x, y, z, 15, 0.7, 0.7, 0.7, 0.2);
+                        if (!world.getEntitiesOfClass(LivingEntity.class, AABB.ofSize(new Vec3(x, y, z), 5, 5, 5), e -> true).isEmpty()) {
+                            speed = speed * 1.2;
+                        }
+                    }
+                }
+            }
 
     }
 
@@ -104,4 +155,9 @@ public class PurgenPiluleProjectile extends ThrowableItemProjectile implements I
     }
 
 
+
+    public void setItem(ItemStack pStack) {
+        super.setItem(pStack);
+
+    }
 }
