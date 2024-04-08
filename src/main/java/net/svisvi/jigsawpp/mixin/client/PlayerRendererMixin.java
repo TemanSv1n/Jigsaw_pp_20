f3291c0ea4c96d2325553408d6d46986dfc73ced
TemanSv1n <temasyr.o@gmail.com>
2024-04-08 23:26:57 +0300

    FCKING MIXINS & purgen gun animation

---
+++ src/main/java/net/svisvi/jigsawpp/mixin/client/PlayerRendererMixin.java
@@ -0,0 +1,27 @@
+package net.svisvi.jigsawpp.mixin.client;
+
+import net.svisvi.jigsawpp.item.ut.CustomArmPoseItem;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+import net.minecraft.client.model.HumanoidModel.ArmPose;
+import net.minecraft.client.player.AbstractClientPlayer;
+import net.minecraft.client.renderer.entity.player.PlayerRenderer;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.item.ItemStack;
+
+@Mixin(PlayerRenderer.class)
+public class PlayerRendererMixin {
+    @Inject(method = "getArmPose(Lnet/minecraft/client/player/AbstractClientPlayer;Lnet/minecraft/world/InteractionHand;)Lnet/minecraft/client/model/HumanoidModel$ArmPose;", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/client/player/AbstractClientPlayer;getItemInHand(Lnet/minecraft/world/InteractionHand;)Lnet/minecraft/world/item/ItemStack;"), locals = LocalCapture.CAPTURE_FAILHARD, cancellable = true)
+    private static void jigsaw$onGetArmPose(AbstractClientPlayer player, InteractionHand hand, CallbackInfoReturnable<ArmPose> cir, ItemStack stack) {
+        if (stack.getItem() instanceof CustomArmPoseItem armPoseProvider) {
+            ArmPose pose = armPoseProvider.getArmPose(stack, player, hand);
+            if (pose != null) {
+                cir.setReturnValue(pose);
+            }
+        }
+    }
+}
