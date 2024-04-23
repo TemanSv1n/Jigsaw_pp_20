a2d54c9744587e9e2ef3599f65f66d551b393121
TemanSv1n <temasyr.o@gmail.com>
2024-04-24 00:22:30 +0300

    Purgen factory recipe CLOSE TO FINAL
    i should only add decorative shit and recipe front is ready!

--- src/main/java/net/svisvi/jigsawpp/compat/PurgenFactoryCategory.java
+++ src/main/java/net/svisvi/jigsawpp/compat/PurgenFactoryCategory.java
@@ -1,26 +1,36 @@
 package net.svisvi.jigsawpp.compat;
 
 import mezz.jei.api.constants.VanillaTypes;
+import mezz.jei.api.forge.ForgeTypes;
 import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
 import mezz.jei.api.gui.drawable.IDrawable;
+import mezz.jei.api.gui.ingredient.IRecipeSlotTooltipCallback;
 import mezz.jei.api.helpers.IGuiHelper;
 import mezz.jei.api.recipe.IFocusGroup;
 import mezz.jei.api.recipe.RecipeIngredientRole;
 import mezz.jei.api.recipe.RecipeType;
 import mezz.jei.api.recipe.category.IRecipeCategory;
+import net.minecraft.ChatFormatting;
 import net.minecraft.data.tags.TagsProvider;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.alchemy.PotionUtils;
 import net.minecraftforge.common.data.ForgeItemTagsProvider;
+import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.svisvi.jigsawpp.JigsawPpMod;
+import net.svisvi.jigsawpp.client.screen.FluidTankRenderer;
 import net.svisvi.jigsawpp.init.ModTags;
 import net.svisvi.jigsawpp.item.init.ModItems;
+import net.svisvi.jigsawpp.item.pilule.AbstractPiluleItem;
 import net.svisvi.jigsawpp.item.purgen_recipe_helpers.AbstractPurgenRecipeHelperItem;
 import net.svisvi.jigsawpp.recipe.PurgenFactoryRecipe;
+
+import java.util.List;
+import java.util.Optional;
 
 public class PurgenFactoryCategory implements IRecipeCategory<PurgenFactoryRecipe> {
     public static final ResourceLocation UID = new ResourceLocation(JigsawPpMod.MODID, "purgen_factory");
@@ -34,6 +44,7 @@ public class PurgenFactoryCategory implements IRecipeCategory<PurgenFactoryRecip
     public PurgenFactoryCategory(IGuiHelper helper) {
         this.background = helper.createDrawable(TEXTURE, 0, 0, 176, 85);
         this.icon = helper.createDrawableIngredient(VanillaTypes.ITEM_STACK, new ItemStack(ModItems.PURGEN_FACTORY.get()));
+
     }
 
     @Override
@@ -58,16 +69,21 @@ public class PurgenFactoryCategory implements IRecipeCategory<PurgenFactoryRecip
 
     @Override
     public void setRecipe(IRecipeLayoutBuilder builder, PurgenFactoryRecipe recipe, IFocusGroup focuses) {
+
         //ingreds
         builder.addSlot(RecipeIngredientRole.INPUT, 26, 6).addIngredients(recipe.getIngredients().get(0));
         builder.addSlot(RecipeIngredientRole.INPUT, 26, 24).addIngredients(recipe.getIngredients().get(1));
 
-        builder.addSlot(RecipeIngredientRole.OUTPUT, 152, 60).addItemStack(recipe.getResultItem(null));
+        ItemStack output_stack = recipe.getResultItem(null);
+        AbstractPiluleItem.setDurationBuff(recipe.getAdditionalTime(null), output_stack);
+        AbstractPiluleItem.setPurity(recipe.getPurity(null), output_stack);
+        PotionUtils.setCustomEffects(output_stack, recipe.getEffects());
 
-        builder.addSlot(RecipeIngredientRole.CATALYST, 134, 41).addItemStack(recipe.getPotionStack());
-//        builder
-//                .addSlot(RecipeIngredientRole.RENDER_ONLY, 80, 96)
-//                .addItemStack(new ItemStack(ModItems.PURGEN_FACTORY_BIG_THUMB.get()));
+        builder.addSlot(RecipeIngredientRole.OUTPUT, 152, 60).addItemStack(output_stack);
+
+        builder
+                .addSlot(RecipeIngredientRole.RENDER_ONLY, 80, 96)
+                .addItemStack(new ItemStack(ModItems.PURGEN_FACTORY_BIG_THUMB.get()));
         //empty pilule decorative
         builder.addSlot(RecipeIngredientRole.INPUT, 152, 29).addItemStack(new ItemStack(ModItems.EMPTY_PILULE.get()));
 
@@ -80,18 +96,37 @@ public class PurgenFactoryCategory implements IRecipeCategory<PurgenFactoryRecip
         AbstractPurgenRecipeHelperItem.setInt(recipe.getPurity(null), purityStack);
 
         ItemStack tntStack = new ItemStack(ModItems.PURGEN_RECIPE_HELPER_TNT.get());
-        AbstractPurgenRecipeHelperItem.setToDisplay("float", tntStack);
-        AbstractPurgenRecipeHelperItem.setFloat(recipe.getMalChance(null), tntStack);
+        AbstractPurgenRecipeHelperItem.setToDisplay("float_alt", tntStack);
+        AbstractPurgenRecipeHelperItem.setFloatAlt(recipe.getMalChance(null), tntStack);
 
         builder
-                .addSlot(RecipeIngredientRole.RENDER_ONLY, 152, 59)
-                .addItemStack(additionalTimeStack);
-        builder
-                .addSlot(RecipeIngredientRole.RENDER_ONLY, 152, 41)
-                .addItemStack(purityStack);
-        builder
-                .addSlot(RecipeIngredientRole.RENDER_ONLY, 152, 23)
+                .addSlot(RecipeIngredientRole.RENDER_ONLY, 134, 60)
                 .addItemStack(tntStack);
 
+        builder
+                .addSlot(RecipeIngredientRole.OUTPUT, 52, 11)
+                .addIngredient(ForgeTypes.FLUID_STACK, recipe.getFluidStack())
+                .addTooltipCallback(addFluidTooltip(recipe.getFluidStack().getAmount()));
+
+    }
+
+    public static IRecipeSlotTooltipCallback addFluidTooltip(int mbAmount) {
+        return (view, tooltip) -> {
+            Optional<FluidStack> displayed = view.getDisplayedIngredient(ForgeTypes.FLUID_STACK);
+            if (displayed.isEmpty())
+                return;
+
+            FluidStack fluidStack = displayed.get();
+
+            int amount = mbAmount == -1 ? fluidStack.getAmount() : mbAmount;
+            Component text = Component.literal(String.valueOf(amount)).append(Component.translatable("jigsaw_pp.tooltip.liquid.mb")).withStyle(ChatFormatting.GOLD);
+            if (tooltip.isEmpty())
+                tooltip.add(0, text);
+            else {
+                List<Component> siblings = tooltip.get(0).getSiblings();
+                siblings.add(Component.literal(" "));
+                siblings.add(text);
+            }
+        };
     }
 }
