fe3b99f6780f728ae59b879cc2c41c3435c4e23e
HLNikNiky <56186901+Sesdear@users.noreply.github.com>
2024-04-28 03:14:40 +0700

    Добавлен - Пустая кега, взрыв для полной кеги?
    Исправленно - Баги с отображением

--- src/main/java/net/svisvi/jigsawpp/item/init/ModItems.java
+++ src/main/java/net/svisvi/jigsawpp/item/init/ModItems.java
@@ -92,6 +92,7 @@ public class ModItems {
 	public static final RegistryObject<Item> BEAWEED_BLOCK = block(ModBlocks.BEAWEED_BLOCK);
 	public static final RegistryObject<Item> FACTORY_HEATER = block(ModBlocks.FACTORY_HEATER);
 	public static final RegistryObject<Item> KEGA = block(ModBlocks.KEGA);
+	public static final RegistryObject<Item> KEGA_NULL = block(ModBlocks.KEGA_NULL);
 
 	public static final RegistryObject<Item> MARMOSET = REGISTRY.register("marmoset", () -> new MarmosetItem());
 
