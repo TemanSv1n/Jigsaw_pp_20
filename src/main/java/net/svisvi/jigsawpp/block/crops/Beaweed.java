1c774e632308bff08c3785ff542e4c448827d840
Pisyapopa777 <hahariashka@yandex.ru>
2024-05-05 20:22:36 +0700

    beaweed fixed

--- src/main/java/net/svisvi/jigsawpp/block/crops/Beaweed.java
+++ src/main/java/net/svisvi/jigsawpp/block/crops/Beaweed.java
@@ -1,12 +1,26 @@
 package net.svisvi.jigsawpp.block.crops;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.particles.SimpleParticleType;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.CropBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.level.storage.loot.LootParams;
 import net.svisvi.jigsawpp.item.init.ModItems;
+import net.svisvi.jigsawpp.particles.ModParticleTypes;
+
+import java.util.Collections;
+import java.util.List;
 
 public class Beaweed extends CropBlock {
     public static final int MAX_AGE = 5;
@@ -30,5 +44,38 @@ public class Beaweed extends CropBlock {
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder){
         pBuilder.add(AGE);
     }
+    @Override
+    protected boolean mayPlaceOn(BlockState pState, BlockGetter pLevel, BlockPos pPos) {
+        return pState.is(BlockTags.DIRT) || pState.is(Blocks.FARMLAND);
+    }
+    @Override
+    public void onPlace(BlockState blockstate, Level world, BlockPos pos, BlockState oldState, boolean moving) {
+        super.onPlace(blockstate, world, pos, oldState, moving);
+        world.scheduleTick(pos, this, 5);
+    }
 
+    @Override
+    public void tick(BlockState blockstate, ServerLevel world, BlockPos pos, RandomSource random) {
+        super.tick(blockstate, world, pos, random);
+        int x = pos.getX();
+        int y = pos.getY();
+        int z = pos.getZ();
+        this.ticking(world, pos, blockstate);
+        world.scheduleTick(pos, this, 5);
+    }
+    public void ticking(Level level, BlockPos pos, BlockState state) {
+        if ((state.getBlock().getStateDefinition().getProperty("age") instanceof IntegerProperty _getip1 ? state.getValue(_getip1) : -1) == 3) {
+            if (level instanceof ServerLevel _level)
+                _level.sendParticles((SimpleParticleType) (ModParticleTypes.FLY.get()), pos.getX(), pos.getY(), pos.getZ(), 5, 3, 3, 3, 0.3);
+        }
+    }
+    @Override
+    public List<ItemStack> getDrops(BlockState state, LootParams.Builder builder) {
+        List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+        if (!dropsOriginal.isEmpty())
+            return dropsOriginal;
+        return Collections.singletonList(new ItemStack(this));
+    }
 }
+
+
