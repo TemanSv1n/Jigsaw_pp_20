1595c16b9735f6847167b6b09e943e8ef597dfff
TemanSv1n <temasyr.o@gmail.com>
2024-08-02 17:43:39 +0300

    Ora ora
    jotaro hat

--- src/main/java/net/svisvi/jigsawpp/event/init/ModEventBusClientEvents.java
+++ src/main/java/net/svisvi/jigsawpp/event/init/ModEventBusClientEvents.java
@@ -8,6 +8,7 @@ import net.svisvi.jigsawpp.JigsawPpMod;
 import net.svisvi.jigsawpp.client.gui.BeaverSODOverlay;
 import net.svisvi.jigsawpp.client.gui.PoopOverlay;
 import net.svisvi.jigsawpp.entity.armor.GasMaskModel;
+import net.svisvi.jigsawpp.entity.armor.JotaroHatModel;
 import net.svisvi.jigsawpp.entity.blabbit.BlabbitModel;
 import net.svisvi.jigsawpp.entity.init.ModModelLayers;
 import net.svisvi.jigsawpp.entity.moss_elephant.MossElephantModel;
@@ -24,6 +25,7 @@ public class ModEventBusClientEvents {
     @SubscribeEvent
     public static void registerLayerDefinitions(EntityRenderersEvent.RegisterLayerDefinitions event) {
         event.registerLayerDefinition(ModModelLayers.GAS_MASK_LAYER, GasMaskModel::createBodyLayer);
+        event.registerLayerDefinition(ModModelLayers.JOTARO_HAT_LAYER, JotaroHatModel::createBodyLayer);
         event.registerLayerDefinition(ModModelLayers.FLOPPA_MISSILE_LAYER, FloppaMissileModel::createBodyLayer);
     }
 
