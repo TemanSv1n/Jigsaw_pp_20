15e7d71b767041de9c4a36d49cd075193d132e0d
Levik269 <142940833+Levik269@users.noreply.github.com>
2024-08-10 01:25:42 +0700

    Перед обдристом
    хуясницы

--- src/main/java/net/svisvi/jigsawpp/block/init/ModBlocks.java
+++ src/main/java/net/svisvi/jigsawpp/block/init/ModBlocks.java
@@ -56,6 +56,7 @@ public class ModBlocks {
     public static final RegistryObject<Block> DIAMOND_IRON_BLOCK = REGISTRY.register("diamond_iron_block", () -> new DiamondIronBlock());
     public static final RegistryObject<Block> BOTTLE_O_PRICEL = REGISTRY.register("bottle_o_pricel", () -> new BottleOPricelBlock());
     public static final RegistryObject<Block> BEAVER_COMPUTER = REGISTRY.register("beaver_computer", () -> new BeaverComputerBlock());
+    public static final RegistryObject<Block> THRON = REGISTRY.register("thron", () -> new ThronBlock());
     public static final RegistryObject<Block> PW_HAT = REGISTRY.register("pw_hat", () -> new PWHatBlock());
     public static final RegistryObject<Block> BLABEGG = REGISTRY.register("blabegg", () -> new BlabeggBlock());
 }
