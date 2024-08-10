862b336ac8e9755c0eac756151ad6aaf197c58d0
igorkll <frerther@gmail.com>
2024-08-10 23:05:57 +0300

    dristo cup works
    FUCK THE CODING

--- src/main/java/net/svisvi/jigsawpp/item/DristoCupItem.java
+++ src/main/java/net/svisvi/jigsawpp/item/DristoCupItem.java
@@ -1,24 +1,44 @@
 package net.svisvi.jigsawpp.item;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.svisvi.jigsawpp.effect.init.ModEffects;
 
 import java.util.List;
 
 public class DristoCupItem extends Item {
     public DristoCupItem() {
-        super(new Properties().stacksTo(56).rarity(Rarity.COMMON)
-                .food((new FoodProperties.Builder()).nutrition(1)
-                        .saturationMod(0.1f).alwaysEat().meat()
-                        .effect(new MobEffectInstance(MobEffects.UNLUCK, 200, 0), 1F).build()));
+        super(new Properties().stacksTo(1).rarity(Rarity.COMMON)
+            .food((new FoodProperties.Builder()).nutrition(-1)
+            .saturationMod(-0.1f).alwaysEat().meat()
+            .effect(new MobEffectInstance(ModEffects.PURGATIVE.get(), 20, 1), 1F)
+            .build()));
+    }
 
+    @Override
+    public ItemStack finishUsingItem(ItemStack pStack, Level pLevel, LivingEntity pLivingEntity) {
+        double x = pLivingEntity.getX();
+        double y = pLivingEntity.getY();
+        double z = pLivingEntity.getZ();
+
+        if (!pLevel.isClientSide()) {
+            pLevel.playSound(null, new BlockPos((int)x, (int)y, (int)z), ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("jigsaw_pp:clown_horn")), SoundSource.PLAYERS, 1, -1);
+        } else {
+            pLevel.playLocalSound(x, y, z, ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("jigsaw_pp:clown_horn")), SoundSource.PLAYERS, 1, -1, false);
+        }
+        return super.finishUsingItem(pStack, pLevel, pLivingEntity);
     }
 
     @Override
